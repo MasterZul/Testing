e28072c8f3a3d294fffcd78f2c53c5c793893f93
MasterZul <zulkifli_master@yahoo.com>
2018-06-23 17:03:59 +0800

    Testing Classification

---
+++ AR Colour Book (Testing)/AR_coloring_page/Assets/ARColor/Scripts/Walk.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class Walk : MonoBehaviour
+{
+
+    public Transform P;
+
+    private Animator _Anim;
+    private NavMeshAgent _Nav;
+    private int i=2;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _Anim = this.GetComponent<Animator>();
+        _Nav = this.GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    void FixedUpdate()
+    {
+        _Anim.SetFloat("Dis",_Nav.remainingDistance);
+    }
+
+    public void StartWalk()
+    {
+        InvokeRepeating("WalkFree", 0, i);
+    }
+
+    void WalkFree()
+    {
+        if (_Anim)
+        {
+            i = Random.Range(0, 5);
+            if (i>2)
+            {
+                float X = Random.Range(-10f,10f);
+                float Z = Random.Range(-10f, 10f);
+                _Nav.SetDestination(new Vector3(X, 0, Z));
+            }
+        }
+    }
+}
---
+++ AR Colour Book (Testing)/AR_coloring_page/Assets/Script/PlayerControllerTransform.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTransform : MonoBehaviour {
+
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public float rotationRate = 360;
+
+    public float moveSpeed = 10;
+
+    Animator animatorgirl;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        animatorgirl  =  GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveAxis = Input.GetAxis(moveInputAxis);
+        float turnAxis = Input.GetAxis(turnInputAxis);
+
+        ApplyInput(moveAxis, turnAxis);
+
+        bool walking = Input.GetKey(KeyCode.UpArrow);
+        bool walkingBack = Input.GetKey(KeyCode.DownArrow);
+
+        Debug.Log(walking);
+
+        if (walking == true || walkingBack == true)
+        {
+            animatorgirl.SetBool("walking", walking);
+            //anigirl.Play();
+        }
+        else
+        {
+
+            animatorgirl.SetBool("walking", walking);
+        }
+
+    }
+
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+
+    private void Move(float Input)
+    {
+        //transform.Translate(Vector3.forward * Input *moveSpeed);
+        rb.AddForce(transform.forward * Input * moveSpeed, ForceMode.Force);
+    }
+
+    private void Turn(float Input)
+    {
+        transform.Rotate(0, Input * rotationRate * Time.deltaTime, 0);
+    }
+}
